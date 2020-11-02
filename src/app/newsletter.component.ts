42ca7223e274a03d615136eef90480df6d5a61b9
UCantBeatThat <hebbarsandesh98@gmail.com>
2020-11-02 14:55:24 +0530

    Dynamic Display of Newsletter Week

--- src/app/newsletter.component.ts
+++ src/app/newsletter.component.ts
@@ -1,9 +1,23 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 
 @Component({
     selector: 'newsletter',
     templateUrl: './newsletter.component.html'
 })
-export class NewsletterComponent{
+export class NewsletterComponent implements OnInit{
+
+    ngOnInit(): void {
+        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var currDate = new Date();
+        var firstDay = currDate.getDate() - currDate.getDay();
+        var lastDay = firstDay+6;
+
+        var month = monthNames[currDate.getMonth()];
+        var year = currDate.getFullYear();
+
+        document.getElementById("current_week").innerHTML = month + " " + firstDay + "-" + lastDay + ", " + year;
+    }
     
+    
+
 }
