2a6486adbcc929bffe0ec56a66be6a382663db81
UCantBeatThat <hebbarsandesh98@gmail.com>
2020-11-02 11:31:05 +0530

    Commiting basic design

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RockFord-University-UI-WebDesign'`, () => {
+  it(`should have as title 'Rockford-University-UI-Design'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RockFord-University-UI-WebDesign');
+    expect(app.title).toEqual('Rockford-University-UI-Design');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('RockFord-University-UI-WebDesign app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Rockford-University-UI-Design app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'RockFord-University-UI-WebDesign';
+export class AppComponent implements OnInit{
+
+  triggerNameDisplay(){
+    let student_name = localStorage.getItem('student_name');
+    if(student_name){
+      document.getElementById("welcome_text").innerHTML = "Welcome, " + JSON.stringify(student_name).replace(/['"]+/g, '');
+    }
+  }
+
+  ngOnInit(): void {
+
+    this.triggerNameDisplay();
+
+  }
+  //title = 'Rockford-University-UI-Design';
+
+  
 }
